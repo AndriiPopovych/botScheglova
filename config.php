e4f28c7c51de6016b26a20ed44b78872e9cf26b8
AndriiPopovych <gunter0104@gmail.com>
2016-09-14 23:42:56 +0300

    change token

--- config.php
+++ config.php
@@ -7,5 +7,5 @@ function __autoload($className)
     include $className . ".php";
 }
 
-define ("TOKEN", "");
-define ("VERIFY_TOKEN", "");
+define ("TOKEN", "EAAY0vpthk7cBAHGNhFuJTSVTSJVEVLRnIZAOZBzYAFdUboZAhjuBzEE3UtsM9J3pdGZANVbzwJtyP5UWx9MF7IjZB8TPnbyRmiEacqLbcr0ZCxMdYWAZBgJPhMfR0lHyjBmmrv9CSgHHaMP7KVzvaGMJOpGGDGsUWqfndq1XkcrZBQZDZD");
+define ("VERIFY_TOKEN", "123");
